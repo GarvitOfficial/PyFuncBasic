8de0d900a9344d443c8149837de6e1fc7c684693
Garvit Bhardwaj <68268393+GarvitOfficial@users.noreply.github.com>
2023-05-17 15:29:13 +0530

    Added Intro and buildInFunc codes

---
+++ Intro.py
@@ -0,0 +1,17 @@
+# what is function
+def whatIsFunc():
+    return "it helps to divide long codes into smaller parts"
+
+# here we re defining the function and in place of funcName we will add name
+def funcName():
+    return "you successfully called the function"
+funcName()
+
+def sum(num1, num2):     #formal argument kyuki hamne formally sir ko btaya ki apkepass doh number ayenge
+    return num1+num2
+
+result = sum(12,21) #actual argument as ye hum kha rhe h krde yrrr not formally
+
+
+# define vala formal 
+# call vala actual parameter
