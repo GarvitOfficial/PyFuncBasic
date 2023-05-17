8250f71ece2a6f1c834072648f8dd63eb1cf649f
Garvit Bhardwaj <68268393+GarvitOfficial@users.noreply.github.com>
2023-05-17 15:30:38 +0530

    added codeine  buildIn.py

---
+++ buildIn.py
@@ -0,0 +1,53 @@
+# Built in func joh bne huye h
+# int,max,round,input,min,range,print,abs,float,len,eval,type
+
+# int: Converts a number or a string into an integer
+number = int(3.14)
+print(number)  # Output: 3
+
+# max: Returns the maximum value from a sequence of numbers
+numbers = [4, 9, 2, 7, 5]
+maximum = max(numbers)
+print(maximum)  # Output: 9
+
+# round: Rounds a number to a specified number of decimal places
+rounded_number = round(3.14159, 2)
+print(rounded_number)  # Output: 3.14
+
+# input: Reads user input from the console
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+# min: Returns the minimum value from a sequence of numbers
+numbers = [4, 9, 2, 7, 5]
+minimum = min(numbers)
+print(minimum)  # Output: 2
+
+# range: Generates a sequence of numbers within a specified range
+numbers = list(range(1, 6))
+print(numbers)  # Output: [1, 2, 3, 4, 5]
+
+# print: Displays output on the console
+print("Hello, world!")
+
+# abs: Returns the absolute value of a number
+absolute_value = abs(-5)
+print(absolute_value)  # Output: 5
+
+# float: Converts a number or a string into a floating-point number
+number = float("3.14")
+print(number)  # Output: 3.14
+
+# len: Returns the number of elements in a sequence or the length of a string
+numbers = [1, 2, 3, 4, 5]
+length = len(numbers)
+print(length)  # Output: 5
+
+# eval: Evaluates a string as a Python expression
+result = eval("2 + 3")
+print(result)  # Output: 5
+
+# type: Returns the type of an object
+value = 42
+data_type = type(value)
+print(data_type)  # Output: <class 'int'>
